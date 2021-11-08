f38fed748ae3c4e042740cd9841fe73b369a1237
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-11-08 09:28:16 -0800

    added back button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -22,15 +22,12 @@
 	import p2 from '$lib/images/thumbs/FB_IMG_2thumb.jpg';
 	import p3 from '$lib/images/thumbs/FB_IMG_3thumb.jpg';
 	import p4 from '$lib/images/thumbs/FB_IMG_4thumb.jpg';
-
 </script>
-
-
 
 <svelte:head>
 	<title>Tobi</title>
 </svelte:head>
-
+<a href="https://frontpage-czi.pages.dev/" ><button>Back</button></a>
 <div class="column">
 	<a href="/pic4zoom"><img src={pic4thumb} alt="fuckit"></a>
 	<a href="/pic5zoom"><img src={pic5thumb} alt="fuckit"></a>
@@ -40,28 +37,31 @@
 	<a href="/pic12zoom"><img src={pic12thumb} alt="fuckit"></a>
 	<a href="/pic4zoom"><img src={pic4thumb} alt="fuckit"></a>
 	<a href="/pic9zoom"><img src={pic9thumb} alt="fuckit"></a>
-<!-- </div>
-
-
-<div class="column"> -->
 	<a href="/pic1_1zoom"><img src={pic1_1thumb} alt="fuckit"></a>
 	<a href="/pic2_1zoom"><img src={pic2_1thumb} alt="fuckit"></a>
 	<a href="/pic3_1zoom"><img src={pic3_1thumb} alt="fuckit"></a>
-
 	<a href="/p1zoom"><img src={p1thumb} alt="fuckit"></a>
 	<a href="/p2zoom"><img src={p2thumb} alt="fuckit"></a>
 	<a href="/p3zoom"><img src={p3thumb} alt="fuckit"></a>
 	<a href="/p4zoom"><img src={p4thumb} alt="fuckit"></a>
 	<a href="/p5zoom"><img src={p5thumb} alt="fuckit"></a>
-<!-- </div>
-
-<div class="column"> -->
 	<a href="/pic13zoom"><img src={p1} alt="fuckit"></a>
 	<a href="/pic14zoom"><img src={p2} alt="fuckit"></a>
 	<a href="/pic15zoom"><img src={p3} alt="fuckit"></a>
 	<a href="/pic16zoom"><img src={p4} alt="fuckit"></a>
 </div>
+<a href="https://frontpage-czi.pages.dev/" ><button>Back</button></a>
 <style>
+
+	button {
+        width: 25%;
+		padding: 8px;
+		border-radius: 7px;
+		background-color: white;
+		font-size: 1.25em;
+		color: black;
+    }
+
 	.column {
 		flex: 25%;
 		padding: 0 4px;
@@ -70,7 +70,7 @@
 	.column img {
 		margin-top: 8px;
 		border-radius: 10%;
-		max-width: 250px;
+		max-width: 225px;
 	}
 
 	@media screen and (max-width: 420px) {
